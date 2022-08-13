b55f66a537d6aa74ce3477352caecc8b5f15842e
Fernando Barbosa <93137762+ferbarbosa@users.noreply.github.com>
2022-08-13 14:19:36 -0300

    a

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -5,7 +5,7 @@ import config from "config";
 function connect() {
   //const dbUri = config.get("dbUri") as string;
 
-  const dbUri = "mongodb://localhost:27017/hokkaido-api"
+  const dbUri = "mongodb+srv://hokkaido_api:1008japa@cluster0.xnsn0m4.mongodb.net/?retryWrites=true&w=majority"
 
   return mongoose
     .connect(dbUri)
